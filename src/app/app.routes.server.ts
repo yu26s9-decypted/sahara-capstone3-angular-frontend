32ff10202a2e48663e98a1d7d35140467306302b
andy <andyt.cyb@gmail.com>
2026-06-20 16:03:32 -0400

    fix product detail route rendering erorr during build

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'product/:slug',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
