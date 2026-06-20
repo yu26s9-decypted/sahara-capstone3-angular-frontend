718edd3b3e0b99d595fd2ea57494ad5f6e352c34
andy <andyt.cyb@gmail.com>
2026-06-20 13:51:56 -0400

    setup

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, sahara-frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Shop');
   });
 });
