e22a1324ec1bb9dd18c166415c8e36f6804aa201
andy <andyt.cyb@gmail.com>
2026-06-20 15:30:37 -0400

    styled product.html

--- src/app/products/products.ts
+++ src/app/products/products.ts
@@ -1,10 +1,11 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { Product } from '../model/product.model';
 import { ProductService } from '../services/product.service';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './products.html',
   styleUrl: './products.css',
 })
