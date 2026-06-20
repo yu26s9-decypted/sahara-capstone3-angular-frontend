e35028d4848b3a4589fe1696b19d933c6758bafb
andy <andyt.cyb@gmail.com>
2026-06-20 19:16:50 -0400

    styled account details page & authenticate navbar account state

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,6 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { UserService } from '../services/userprofile.service';
+import { AccountDetail } from '../component/account-detail/account-detail';
+import { Profile } from '../model/userprofile.model';
 
 @Component({
   selector: 'app-navbar',
@@ -8,6 +11,18 @@ import { AuthService } from '../services/auth.service';
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {
+export class Navbar implements OnInit {
   authService = inject(AuthService);
+  userService = inject(UserService);
+  profile = signal<Profile | null>(null)
+
+  ngOnInit(): void {
+    if(this.authService.isAuthenticated()){
+      this.userService.getUserProfile().subscribe({
+        next: (profile) => this.profile.set(profile)
+      });
+    }
+  }
 }
+
+
