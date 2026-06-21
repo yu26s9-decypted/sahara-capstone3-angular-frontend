a1e0fd084648d38b139d8fd84ff06c3a1af8c026
andy <andyt.cyb@gmail.com>
2026-06-21 02:20:24 -0400

    added stripePublishableKey to env prod

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
-    baseURL: 'https://sahara-backend-api-capstone.onrender.com'
+    baseURL: 'https://sahara-backend-api-capstone.onrender.com',
+    stripePublishableKey:'pk_test_51TkdqFFXtxuP1VoAcG6JEewAXVmjdOWVXBYyxgOvIAAkATvpsNHtvvDJeD2nCK3HOhmnaXLnYCvRy9UWoe0rfV2K009MZw11p4'
 };
 
